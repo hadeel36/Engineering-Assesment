ae92b86ba0433b2a63f974d32c881dfba1042ae0
Hadeel Khawaldeh <hadeel.khawaldeh@baaz.com>
2017-10-09 11:28:46 +0300

    add test case to app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,18 +7,12 @@ import { OffersService } from './app.service';
 describe('AppComponent', () => {
   beforeEach(() => {
 
-    class OffersStub {
-      getAllOffers = {
-        subscribe: () => {}
-      };
-    }
-
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
       providers: [
-        { provide: OffersService, useClass: OffersStub }
+        { provide: OffersService }
       ]
     });
     TestBed.compileComponents();
@@ -30,16 +24,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  // it(`should have as title 'app works!'`, async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('app works!');
-  // }));
-
-  // it('should render title in a h1 tag', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  // }));
+  it('It should round to integer number', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.getRating(3.4)).toEqual(3);
+  }));
 });
